828e115b0812403ede306544ff485c7061999abe
pranav nikam <pnikam2@huskers.unl.edu>
2021-08-27 00:38:25 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,17 +1,17 @@
     /**
      * author: pranav nikam
      * email:- pnikam2@unl.edu
-     * 
+     *
      * date:-08/24/21
      * a introduction program
      */
      #include<stdlib.h>
       #include<stdio.h>
-      
+
       int main(int argc, char **argv){
-          
+
           printf("pranav nikam\n");
-          
+          printf("major: computer science\n");
+
           return 0;
       }
-      
